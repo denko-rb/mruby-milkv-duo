160d806137a51d7c24d9fbd5a8f90e2b978e0a12
vickash <vickashmahabir@gmail.com>
2024-10-07 23:21:55 -0400

    Change "mrb_wiringx_" to "mrbWX_"

--- src/mrb_milkv_wiringx.c
+++ src/mrb_milkv_wiringx.c
@@ -9,7 +9,7 @@
 #define _milkv_duos     "milkv_duos"
 
 static mrb_value
-mrb_wiringx_setup(mrb_state* mrb, mrb_value self) {
+mrbWX_setup(mrb_state* mrb, mrb_value self) {
   int result = wiringXSetup(MILKV_DUO_VARIANT, NULL);
   if (result == -1) {
     wiringXGC();
@@ -19,7 +19,15 @@ mrb_wiringx_setup(mrb_state* mrb, mrb_value self) {
 }
 
 static mrb_value
-mrb_wiringx_pin_mode(mrb_state* mrb, mrb_value self) {
+mrbWX_valid_gpio(mrb_state* mrb, mrb_value self) {
+  mrb_int pin, valid;
+  mrb_get_args(mrb, "i", &pin);
+  valid = wiringXValidGPIO(pin);
+  return mrb_fixnum_value(valid);
+}
+
+static mrb_value
+mrbWX_pin_mode(mrb_state* mrb, mrb_value self) {
   mrb_int pin, mode;
   mrb_get_args(mrb, "ii", &pin, &mode);
   pinMode(pin, mode);
@@ -27,7 +35,7 @@ mrb_wiringx_pin_mode(mrb_state* mrb, mrb_value self) {
 }
 
 static mrb_value
-mrb_wiringx_digital_write(mrb_state* mrb, mrb_value self) {
+mrbWX_digital_write(mrb_state* mrb, mrb_value self) {
   mrb_int pin, state;
   mrb_get_args(mrb, "ii", &pin, &state);
   digitalWrite(pin, state);
@@ -35,7 +43,7 @@ mrb_wiringx_digital_write(mrb_state* mrb, mrb_value self) {
 }
 
 static mrb_value
-mrb_wiringx_digital_read(mrb_state* mrb, mrb_value self) {
+mrbWX_digital_read(mrb_state* mrb, mrb_value self) {
   mrb_int pin, state;
   mrb_get_args(mrb, "i", &pin);
   state = digitalRead(pin);
@@ -45,23 +53,26 @@ mrb_wiringx_digital_read(mrb_state* mrb, mrb_value self) {
 void
 mrb_mruby_milkv_wiringx_gem_init(mrb_state* mrb) {
   // Module
-  struct RClass *mrb_WiringX = mrb_define_module(mrb, "WiringX");
+  struct RClass *mrbWX = mrb_define_module(mrb, "WiringX");
 
   // Constants
-  mrb_define_const(mrb, mrb_WiringX, "PINMODE_NOT_SET",   mrb_fixnum_value(PINMODE_NOT_SET));
-  mrb_define_const(mrb, mrb_WiringX, "PINMODE_INPUT",     mrb_fixnum_value(PINMODE_INPUT));
-  mrb_define_const(mrb, mrb_WiringX, "PINMODE_OUTPUT",    mrb_fixnum_value(PINMODE_OUTPUT));
-  mrb_define_const(mrb, mrb_WiringX, "PINMODE_INTERRUPT", mrb_fixnum_value(PINMODE_INTERRUPT));
-  mrb_define_const(mrb, mrb_WiringX, "PINMODE_INTERRUPT", mrb_fixnum_value(PINMODE_INTERRUPT));
-  mrb_define_const(mrb, mrb_WiringX, "PINMODE_INTERRUPT", mrb_fixnum_value(PINMODE_INTERRUPT));
-  mrb_define_const(mrb, mrb_WiringX, "LOW",               mrb_fixnum_value(LOW));
-  mrb_define_const(mrb, mrb_WiringX, "HIGH",              mrb_fixnum_value(HIGH));
+  mrb_define_const(mrb, mrbWX, "PINMODE_NOT_SET",   mrb_fixnum_value(PINMODE_NOT_SET));
+  mrb_define_const(mrb, mrbWX, "PINMODE_INPUT",     mrb_fixnum_value(PINMODE_INPUT));
+  mrb_define_const(mrb, mrbWX, "PINMODE_OUTPUT",    mrb_fixnum_value(PINMODE_OUTPUT));
+  mrb_define_const(mrb, mrbWX, "PINMODE_INTERRUPT", mrb_fixnum_value(PINMODE_INTERRUPT));
+  mrb_define_const(mrb, mrbWX, "PINMODE_INTERRUPT", mrb_fixnum_value(PINMODE_INTERRUPT));
+  mrb_define_const(mrb, mrbWX, "PINMODE_INTERRUPT", mrb_fixnum_value(PINMODE_INTERRUPT));
+  mrb_define_const(mrb, mrbWX, "LOW",               mrb_fixnum_value(LOW));
+  mrb_define_const(mrb, mrbWX, "HIGH",              mrb_fixnum_value(HIGH));
 
   // Class Methods
-  mrb_define_method(mrb, mrb_WiringX, "wiringx_setup",  mrb_wiringx_setup,          MRB_ARGS_REQ(0));
-  mrb_define_method(mrb, mrb_WiringX, "pin_mode",       mrb_wiringx_pin_mode,       MRB_ARGS_REQ(2));
-  mrb_define_method(mrb, mrb_WiringX, "digital_write",  mrb_wiringx_digital_write,  MRB_ARGS_REQ(2));
-  mrb_define_method(mrb, mrb_WiringX, "digital_read",   mrb_wiringx_digital_read,   MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, mrbWX, "wiringx_setup",  mrbWX_setup,          MRB_ARGS_REQ(0));
+  mrb_define_method(mrb, mrbWX, "valid_gpio",     mrbWX_valid_gpio,     MRB_ARGS_REQ(1));
+
+  // Digital I/O
+  mrb_define_method(mrb, mrbWX, "pin_mode",       mrbWX_pin_mode,       MRB_ARGS_REQ(2));
+  mrb_define_method(mrb, mrbWX, "digital_write",  mrbWX_digital_write,  MRB_ARGS_REQ(2));
+  mrb_define_method(mrb, mrbWX, "digital_read",   mrbWX_digital_read,   MRB_ARGS_REQ(1));
 }
 
 void
