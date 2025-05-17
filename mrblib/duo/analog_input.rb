4fcc829a7b9fcb9d7562fe4fde97c4e0acb9099c
vickash <vickashmahabir@gmail.com>
2025-05-17 12:05:38 -0400

    Make SARADC setup methods module functions

--- mrblib/duo/analog_input.rb
+++ mrblib/duo/analog_input.rb
@@ -1,7 +1,7 @@
 module Duo
   ADC_PATH = "/sys/class/cvi-saradc/cvi-saradc0/device/cv_saradc"
 
-  def self.saradc_initialize
+  def saradc_initialize
     return true if self.saradc_driver_loaded
 
     puts "SARADC driver not loaded. Searching..."
@@ -14,16 +14,18 @@ module Duo
     return true
   end
 
-  def self.saradc_driver_loaded
+  def saradc_driver_loaded
     result = `lsmod | grep -E "cv180x_saradc|cv181x_saradc"`
     result = false if result.empty?
     result
   end
 
-  def self.find_saradc_driver
+  def find_saradc_driver
     result = `find / -name "cv180x_saradc.ko" 2>/dev/null`
     result = `find / -name "cv181x_saradc.ko" 2>/dev/null` if result.empty?
     result = nil if result.empty?
     result
   end
+
+  module_function :saradc_initialize, :saradc_driver_loaded, :find_saradc_driver
 end
